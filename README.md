613b5dac3659e6a1054031664066d06678588f07
Scripter17 <jameschristopherwise@gmail.com>
2018-11-28 15:01:14 -0500

    Added the DBaD license, and fixed the icon.

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 It's Pong made in HTML.\
 That's really about it.
 
+Press [W] and [S] to move the left paddle, and [Up] and [Down] for the right paddle. First person to 10 points wins.
+
+Released under ["Don't Be a Dick"](https://dbad-license.org/) license.
+
 [Check it out!](https://scripter17.github.io/htmlpong)
--- main.js
+++ main.js
@@ -1,102 +1,153 @@
 window.onload=function(){
-	window.p1=document.getElementById("paddle1");
-	window.p2=document.getElementById("paddle2");
-	window.p1vel=0;
-	window.p2vel=0;
-	window.down={87:false,83:false,38:false,40:false};
-	window.ballv=[];
-	window.ball=document.getElementById("ball");
-	window.speed=5;
-	window.points=[0,0];
-	window.finished=false;
-	window.winscore=10;
+	game={
+		// Held keys
+		down:{87:false, 83:false, 38:false, 40:false},
+		// Paddles
+		p1:document.getElementById("paddle1"),
+		p2:document.getElementById("paddle2"),
+		// Movement speeds of each paddle (1=down, -1=up, 0=stop)
+		p1vel:0,
+		p2vel:0,
+		speed:5, // Pixels per frame (p1.style.top+=p1vel*speed;)
+		// The ball
+		ball:document.getElementById("ball"),
+		ballv:[0,0], // Velocities
+		mag:0,
+		bounce:true,
+		// Points
+		winscore:10,
+		points:[0, 0],
+		finished:false
+	}
+	game.p1.style.top=window.innerHeight/8*3+"px"; // TODO: Prove that this is in the center exactly
+	game.p2.style.top=window.innerHeight/8*3+"px";
+	// Function stuff
 	p1move();
 	p2move();
 	ballInit();
 	doBallStuff();
+}
+window.onresize=function(){
+	alert("Please don't resize the screen.\nIt's very buggy at the moment.");
 }
 window.onkeydown=function(e){
 	// w=87
 	// s=83
 	// up=38
 	// down=40
-	window.down[e.keyCode]=true;
+	game.down[e.keyCode]=true;
 	if (e.keyCode==87){
-		window.p1vel=-1;
+		game.p1vel=-1;
 	} else if (e.keyCode==83){
-		window.p1vel=1;
+		game.p1vel=1;
 	} else if (e.keyCode==38){
-		window.p2vel=-1;
+		game.p2vel=-1;
 	} else if (e.keyCode==40){
-		window.p2vel=1;
+		game.p2vel=1;
 	}
 }
 window.onkeyup=function(e){
-	window.down[e.keyCode]=false;
+	game.down[e.keyCode]=false;
 	if (e.keyCode==87){
-		window.p1vel=(window.down[83]?1:0);
+		game.p1vel=(game.down[83]?1:0);
 	} else if (e.keyCode==83){
-		window.p1vel=(window.down[87]?-1:0);
+		game.p1vel=(game.down[87]?-1:0);
 	} else if (e.keyCode==38){
-		window.p2vel=(window.down[40]?1:0);
+		game.p2vel=(game.down[40]?1:0);
 	} else if (e.keyCode==40){
-		window.p2vel=(window.down[38]?-1:0);
+		game.p2vel=(game.down[38]?-1:0);
 	}
 }
 function p1move(){
-	var top=parseInt(p1.style.top)+window.p1vel*window.speed;
-	var rect=p1.getBoundingClientRect();
+	var rect=game.p1.getBoundingClientRect();
+	// Get new distance from top
+	var top=parseInt(game.p1.style.top)+game.p1vel*game.speed;
 	top=Math.max(top,0);
 	top=Math.min(window.innerHeight-rect.height,top);
-	p1.style.top=top+"px";
+	game.p1.style.top=top+"px";
+	
 	requestAnimationFrame(p1move);
 }
 function p2move(){
-	var top=parseInt(p2.style.top)+window.p2vel*window.speed;
-	var rect=p2.getBoundingClientRect();
+	var rect=game.p2.getBoundingClientRect();
+	// Get new distance from top
+	var top=parseInt(game.p2.style.top)+game.p2vel*game.speed;
 	top=Math.max(top,0);
 	top=Math.min(window.innerHeight-rect.height,top);
-	p2.style.top=top+"px";
+	game.p2.style.top=top+"px";
+	
 	requestAnimationFrame(p2move);
 }
 function ballInit(){
-	window.ball.style.left=window.innerWidth/2+"px";
-	window.ball.style.top=window.innerHeight/2+"px";
-	var genv=function(){return (Math.random()>0.5?-1:1)*Math.ceil(Math.random()*3+2);}
-	window.ballv=[genv(),genv()];
+	// var genv=function(){return (Math.random()>0.5?-1:1)*Math.ceil(Math.random()*3+2);}
+	var genv=function(){return [-5, -4, -3, 3, 4, 5][Math.floor(Math.random()*6)];} // Velocity generator function (Equivalent to above)
+	// Put ball in the center of the screen
+	game.ball.style.left=window.innerWidth/2+"px";
+	game.ball.style.top=window.innerHeight/2+"px";
+	// Randomly generate the velocity of the ball
+	game.ballv=[genv(), genv()];
+	game.mag=Math.sqrt(game.ballv[0]**2+game.ballv[1]**2);
 }
 function doBallStuff(){
-	var ballRect=window.ball.getBoundingClientRect();
-	var p1Rect=window.p1.getBoundingClientRect();
-	var p2Rect=window.p2.getBoundingClientRect();
-	if (!(p1Rect.right<ballRect.left||p1Rect.left>ballRect.right||p1Rect.bottom<ballRect.top||p1Rect.top>ballRect.bottom)){ballv[0]*=-1;}
-	if (!(p2Rect.right<ballRect.left||p2Rect.left>ballRect.right||p2Rect.bottom<ballRect.top||p2Rect.top>ballRect.bottom)){ballv[0]*=-1;}
-	var left=parseFloat(window.ball.style.left)+window.ballv[0];
-	var leftpre=left;
-	left=Math.min(window.innerWidth-ballRect.width,left);
+	var ballRect=game.ball.getBoundingClientRect();
+	var p1Rect=game.p1.getBoundingClientRect();
+	var p2Rect=game.p2.getBoundingClientRect();
+	var left, leftpre, top, toppre;
+	if ( // https://stackoverflow.com/a/12067046
+		(!(p1Rect.right<ballRect.left||p1Rect.left>ballRect.right||p1Rect.bottom<ballRect.top||p1Rect.top>ballRect.bottom)
+		|| !(p2Rect.right<ballRect.left||p2Rect.left>ballRect.right||p2Rect.bottom<ballRect.top||p2Rect.top>ballRect.bottom))
+		&& game.bounce
+	){
+		// The game.bounce shenanigans stop the ball from jittering when it hits a paddle from the side
+		game.bounce=false;
+		
+		game.ballv[0]*=-1
+		// TODO: Replace the above with a fixed version of the below.
+		/*var ballhmid=(ballRect.top+ballRect.bottom)/2
+		if (ballRect.left<window.innerWidth/2){
+			var padhmid=(p1Rect.top+p1Rect.bottom)/2;
+			var angle=Math.atan2(ballhmid-padhmid, ballRect.left-p1Rect.left)/6
+			console.log(1, angle, game.mag)
+			game.ballv=[Math.cos(angle*game.mag), Math.sin(angle)*game.mag]
+		} else {
+			var padhmid=(p2Rect.top+p2Rect.bottom)/2;
+			var angle=Math.atan2(ballhmid-padhmid, ballRect.right-p2Rect.right)/6
+			console.log(2, angle, game.mag)
+			game.ballv=[Math.cos(angle)*game.mag, Math.sin(angle)*game.mag]
+		}*/
+		
+		setTimeout(function(){game.bounce=true;}, 2500);
+	}
+	// Calculate new distance from the left
+	left=parseFloat(game.ball.style.left)+game.ballv[0]; // New distance
+	leftpre=left; // Old distance
+	left=Math.min(window.innerWidth-ballRect.width, left);
 	left=Math.max(left,0);
 	if (leftpre!=left){ // Point scored
-		window.points[left==0?1:0]++;
+		// This code only activates when the ball would've gone off-screen (i.e: When a point is scored)
+		game.points[left==0?1:0]++;
 		updatePoints();
 		ballInit();
 	} else {
-		window.ball.style.left=left+"px";
-		var top=parseFloat(window.ball.style.top)+window.ballv[1];
-		var toppre=top;
+		game.ball.style.left=left+"px";
+		// Calculate new distance from top
+		top=parseFloat(game.ball.style.top)+game.ballv[1]; // New distance
+		toppre=top; // Old distance
 		top=Math.min(window.innerHeight-ballRect.height,top);
 		top=Math.max(top,0);
-		if (toppre!=top){ballv[1]*=-1}
-		window.ball.style.top=top+"px";
+		
+		if (toppre!=top){game.ballv[1]*=-1}
+		game.ball.style.top=top+"px";
 	}
-	if (!window.finished){requestAnimationFrame(doBallStuff);}
+	if (!game.finished){requestAnimationFrame(doBallStuff);}
 }
 function updatePoints(){
 	var score1=document.getElementById("score1");
 	var score2=document.getElementById("score2");
-	score1.innerHTML=window.points[0]
-	score2.innerHTML=window.points[1]
-	if (points.indexOf(window.winscore)!=-1){
-		window.finished=true;
-		document.getElementById("winner").innerHTML="Player "+(points.indexOf(window.winscore)+1)+" wins!";
+	score1.innerHTML=game.points[0];
+	score2.innerHTML=game.points[1];
+	if (game.points.indexOf(game.winscore)!=-1){ // Has either player reached the winning score?
+		game.finished=true;
+		document.getElementById("winner").innerHTML="Player "+(game.points.indexOf(game.winscore)+1)+" wins!";
 	}
 }
